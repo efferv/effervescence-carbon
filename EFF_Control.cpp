4a685e44fa261493ca4d747e4080375f47501202
nerrons <coniinedieu@gmail.com>
2020-03-25 02:23:21 +0800

    Add EFF_Control

---
+++ EFF_Control.cpp
@@ -0,0 +1,176 @@
+//
+//  EFF_Control.cpp
+//  effervescence-driver
+//
+//  Created by Nerrons on 25/3/20.
+//  Copyright © 2020 nerrons. All rights reserved.
+//
+
+// Self Include
+#include "EFF_Control.h"
+
+// PublicUtility Includes
+#include "CADebugMacros.h"
+#include "CAException.h"
+
+// System Includes
+#include <CoreAudio/AudioHardwareBase.h>
+
+
+#pragma mark Constructor/Destructor
+
+EFF_Control::EFF_Control(AudioObjectID inObjectID,
+                         AudioClassID inClassID,
+                         AudioClassID inBaseClassID,
+                         AudioObjectID inOwnerObjectID,
+                         AudioObjectPropertyScope inScope,
+                         AudioObjectPropertyElement inElement)
+:
+    EFF_Object(inObjectID, inClassID, inBaseClassID, inOwnerObjectID),
+    mScope(inScope),
+    mElement(inElement)
+{
+}
+
+
+#pragma mark Property Operations
+
+bool    EFF_Control::HasProperty(AudioObjectID inObjectID,
+                                 pid_t inClientPID,
+                                 const AudioObjectPropertyAddress& inAddress)
+const
+{
+    CheckObjectID(inObjectID);
+
+    bool theAnswer = false;
+
+    switch(inAddress.mSelector)
+    {
+        case kAudioControlPropertyScope:
+        case kAudioControlPropertyElement:
+            theAnswer = true;
+            break;
+
+        default:
+            theAnswer = EFF_Object::HasProperty(inObjectID, inClientPID, inAddress);
+            break;
+    };
+
+    return theAnswer;
+}
+
+bool    EFF_Control::IsPropertySettable(AudioObjectID inObjectID,
+                                        pid_t inClientPID,
+                                        const AudioObjectPropertyAddress& inAddress)
+const
+{
+    CheckObjectID(inObjectID);
+
+    bool theAnswer = false;
+
+    switch(inAddress.mSelector)
+    {
+        case kAudioControlPropertyScope:
+        case kAudioControlPropertyElement:
+            theAnswer = false;
+            break;
+
+        default:
+            theAnswer = EFF_Object::IsPropertySettable(inObjectID,
+                                                       inClientPID,
+                                                       inAddress);
+            break;
+    };
+
+    return theAnswer;
+}
+
+UInt32  EFF_Control::GetPropertyDataSize(AudioObjectID inObjectID,
+                                         pid_t inClientPID,
+                                         const AudioObjectPropertyAddress& inAddress,
+                                         UInt32 inQualifierDataSize,
+                                         const void* inQualifierData)
+const
+{
+    CheckObjectID(inObjectID);
+
+    UInt32 theAnswer = 0;
+
+    switch(inAddress.mSelector)
+    {
+        case kAudioControlPropertyScope:
+            theAnswer = sizeof(AudioObjectPropertyScope);
+            break;
+
+        case kAudioControlPropertyElement:
+            theAnswer = sizeof(AudioObjectPropertyElement);
+            break;
+
+        default:
+            theAnswer = EFF_Object::GetPropertyDataSize(inObjectID,
+                                                        inClientPID,
+                                                        inAddress,
+                                                        inQualifierDataSize,
+                                                        inQualifierData);
+            break;
+    };
+
+    return theAnswer;
+}
+
+void    EFF_Control::GetPropertyData(AudioObjectID inObjectID,
+                                     pid_t inClientPID,
+                                     const AudioObjectPropertyAddress& inAddress,
+                                     UInt32 inQualifierDataSize,
+                                     const void* inQualifierData,
+                                     UInt32 inDataSize,
+                                     UInt32& outDataSize,
+                                     void* outData)
+const
+{
+    CheckObjectID(inObjectID);
+
+    switch(inAddress.mSelector)
+    {
+        case kAudioControlPropertyScope:
+            // This property returns the scope that the control is attached to.
+            ThrowIf(inDataSize < sizeof(AudioObjectPropertyScope),
+                    CAException(kAudioHardwareBadPropertySizeError),
+                    "EFF_Control::GetPropertyData: not enough space for the return value of "
+                    "kAudioControlPropertyScope for the control");
+            *reinterpret_cast<AudioObjectPropertyScope*>(outData) = mScope;
+            outDataSize = sizeof(AudioObjectPropertyScope);
+            break;
+
+        case kAudioControlPropertyElement:
+            // This property returns the element that the control is attached to.
+            ThrowIf(inDataSize < sizeof(AudioObjectPropertyElement),
+                    CAException(kAudioHardwareBadPropertySizeError),
+                    "EFF_Control::GetPropertyData: not enough space for the return value of "
+                    "kAudioControlPropertyElement for the control");
+            *reinterpret_cast<AudioObjectPropertyElement*>(outData) = mElement;
+            outDataSize = sizeof(AudioObjectPropertyElement);
+            break;
+
+        default:
+            EFF_Object::GetPropertyData(inObjectID,
+                                        inClientPID,
+                                        inAddress,
+                                        inQualifierDataSize,
+                                        inQualifierData,
+                                        inDataSize,
+                                        outDataSize,
+                                        outData);
+            break;
+    };
+}
+
+void    EFF_Control::CheckObjectID(AudioObjectID inObjectID)
+const
+{
+    ThrowIf(inObjectID == kAudioObjectUnknown || inObjectID != GetObjectID(),
+            CAException(kAudioHardwareBadObjectError),
+            "EFF_Control::CheckObjectID: wrong audio object ID for the control");
+}
+
+#pragma clang assume_nonnull end
---
+++ EFF_Control.h
@@ -0,0 +1,72 @@
+//
+//  EFF_Control.h
+//  effervescence-core
+//
+//  Created by Nerrons on 25/3/20.
+//  Copyright © 2020 nerrons. All rights reserved.
+//
+
+#ifndef EFF_Control_h
+#define EFF_Control_h
+
+// Superclass Includes
+#include "EFF_Object.h"
+
+
+#pragma clang assume_nonnull begin
+
+class EFF_Control
+:
+    public EFF_Object
+{
+
+    
+#pragma mark Constructor/Destructor
+    
+protected:
+                        EFF_Control(AudioObjectID inObjectID,
+                                    AudioClassID inClassID,
+                                    AudioClassID inBaseClassID,
+                                    AudioObjectID inOwnerObjectID,
+                                    AudioObjectPropertyScope inScope =
+                                            kAudioObjectPropertyScopeOutput,
+                                    AudioObjectPropertyElement inElement =
+                                            kAudioObjectPropertyElementMaster);
+    
+    
+#pragma mark Property Operations
+
+public:
+    virtual bool        HasProperty(AudioObjectID inObjectID,
+                                    pid_t inClientPID,
+                                    const AudioObjectPropertyAddress& inAddress) const;
+    virtual bool        IsPropertySettable(AudioObjectID inObjectID,
+                                           pid_t inClientPID,
+                                           const AudioObjectPropertyAddress& inAddress) const;
+    virtual UInt32      GetPropertyDataSize(AudioObjectID inObjectID,
+                                            pid_t inClientPID,
+                                            const AudioObjectPropertyAddress& inAddress,
+                                            UInt32 inQualifierDataSize,
+                                            const void* inQualifierData) const;
+    virtual void        GetPropertyData(AudioObjectID inObjectID,
+                                        pid_t inClientPID,
+                                        const AudioObjectPropertyAddress& inAddress,
+                                        UInt32 inQualifierDataSize,
+                                        const void* inQualifierData,
+                                        UInt32 inDataSize,
+                                        UInt32& outDataSize,
+                                        void* outData) const;
+
+
+#pragma mark Implementation
+
+protected:
+    void                                CheckObjectID(AudioObjectID inObjectID) const;
+    const AudioObjectPropertyScope      mScope;
+    const AudioObjectPropertyElement    mElement;
+
+};
+
+#pragma clang assume_nonnull end
+
+#endif /* EFF_Control_h */
