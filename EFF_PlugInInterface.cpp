79a5f457255acf8f94002e458aa34f35579966e1
nerrons <coniinedieu@gmail.com>
2020-03-25 00:09:45 +0800

    Stylize EFF_PlugInInterface

--- EFF_PlugInInterface.cpp
+++ EFF_PlugInInterface.cpp
@@ -23,29 +23,107 @@
 #pragma mark COM Prototypes
 
 // Entry points for the COM methods
-extern "C" void*    EFF_Create(CFAllocatorRef inAllocator, CFUUIDRef inRequestedTypeUUID);
-static HRESULT      EFF_QueryInterface(void* inDriver, REFIID inUUID, LPVOID* outInterface);
+extern "C" void*    EFF_Create(CFAllocatorRef inAllocator,
+                               CFUUIDRef inRequestedTypeUUID);
+static HRESULT      EFF_QueryInterface(void* inDriver,
+                                       REFIID inUUID,
+                                       LPVOID* outInterface);
 static ULONG        EFF_AddRef(void* inDriver);
 static ULONG        EFF_Release(void* inDriver);
-static OSStatus     EFF_Initialize(AudioServerPlugInDriverRef inDriver, AudioServerPlugInHostRef inHost);
-static OSStatus     EFF_CreateDevice(AudioServerPlugInDriverRef inDriver, CFDictionaryRef inDescription, const AudioServerPlugInClientInfo* inClientInfo, AudioObjectID* outDeviceObjectID);
-static OSStatus     EFF_DestroyDevice(AudioServerPlugInDriverRef inDriver, AudioObjectID inDeviceObjectID);
-static OSStatus     EFF_AddDeviceClient(AudioServerPlugInDriverRef inDriver, AudioObjectID inDeviceObjectID, const AudioServerPlugInClientInfo* inClientInfo);
-static OSStatus     EFF_RemoveDeviceClient(AudioServerPlugInDriverRef inDriver, AudioObjectID inDeviceObjectID, const AudioServerPlugInClientInfo* inClientInfo);
-static OSStatus     EFF_PerformDeviceConfigurationChange(AudioServerPlugInDriverRef inDriver, AudioObjectID inDeviceObjectID, UInt64 inChangeAction, void* inChangeInfo);
-static OSStatus     EFF_AbortDeviceConfigurationChange(AudioServerPlugInDriverRef inDriver, AudioObjectID inDeviceObjectID, UInt64 inChangeAction, void* inChangeInfo);
-static Boolean      EFF_HasProperty(AudioServerPlugInDriverRef inDriver, AudioObjectID inObjectID, pid_t inClientProcessID, const AudioObjectPropertyAddress* inAddress);
-static OSStatus     EFF_IsPropertySettable(AudioServerPlugInDriverRef inDriver, AudioObjectID inObjectID, pid_t inClientProcessID, const AudioObjectPropertyAddress* inAddress, Boolean* outIsSettable);
-static OSStatus     EFF_GetPropertyDataSize(AudioServerPlugInDriverRef inDriver, AudioObjectID inObjectID, pid_t inClientProcessID, const AudioObjectPropertyAddress* inAddress, UInt32 inQualifierDataSize, const void* inQualifierData, UInt32* outDataSize);
-static OSStatus     EFF_GetPropertyData(AudioServerPlugInDriverRef inDriver, AudioObjectID inObjectID, pid_t inClientProcessID, const AudioObjectPropertyAddress* inAddress, UInt32 inQualifierDataSize, const void* inQualifierData, UInt32 inDataSize, UInt32* outDataSize, void* outData);
-static OSStatus     EFF_SetPropertyData(AudioServerPlugInDriverRef inDriver, AudioObjectID inObjectID, pid_t inClientProcessID, const AudioObjectPropertyAddress* inAddress, UInt32 inQualifierDataSize, const void* inQualifierData, UInt32 inDataSize, const void* inData);
-static OSStatus     EFF_StartIO(AudioServerPlugInDriverRef inDriver, AudioObjectID inDeviceObjectID, UInt32 inClientID);
-static OSStatus     EFF_StopIO(AudioServerPlugInDriverRef inDriver, AudioObjectID inDeviceObjectID, UInt32 inClientID);
-static OSStatus     EFF_GetZeroTimeStamp(AudioServerPlugInDriverRef inDriver, AudioObjectID inDeviceObjectID, UInt32 inClientID, Float64* outSampleTime, UInt64* outHostTime, UInt64* outSeed);
-static OSStatus     EFF_WillDoIOOperation(AudioServerPlugInDriverRef inDriver, AudioObjectID inDeviceObjectID, UInt32 inClientID, UInt32 inOperationID, Boolean* outWillDo, Boolean* outWillDoInPlace);
-static OSStatus     EFF_BeginIOOperation(AudioServerPlugInDriverRef inDriver, AudioObjectID inDeviceObjectID, UInt32 inClientID, UInt32 inOperationID, UInt32 inIOBufferFrameSize, const AudioServerPlugInIOCycleInfo* inIOCycleInfo);
-static OSStatus     EFF_DoIOOperation(AudioServerPlugInDriverRef inDriver, AudioObjectID inDeviceObjectID, AudioObjectID inStreamObjectID, UInt32 inClientID, UInt32 inOperationID, UInt32 inIOBufferFrameSize, const AudioServerPlugInIOCycleInfo* inIOCycleInfo, void* ioMainBuffer, void* ioSecondaryBuffer);
-static OSStatus     EFF_EndIOOperation(AudioServerPlugInDriverRef inDriver, AudioObjectID inDeviceObjectID, UInt32 inClientID, UInt32 inOperationID, UInt32 inIOBufferFrameSize, const AudioServerPlugInIOCycleInfo* inIOCycleInfo);
+static OSStatus     EFF_Initialize(AudioServerPlugInDriverRef inDriver,
+                                   AudioServerPlugInHostRef inHost);
+static OSStatus     EFF_CreateDevice(AudioServerPlugInDriverRef inDriver,
+                                     CFDictionaryRef inDescription,
+                                     const AudioServerPlugInClientInfo* inClientInfo,
+                                     AudioObjectID* outDeviceObjectID);
+static OSStatus     EFF_DestroyDevice(AudioServerPlugInDriverRef inDriver,
+                                      AudioObjectID inDeviceObjectID);
+static OSStatus     EFF_AddDeviceClient(AudioServerPlugInDriverRef inDriver,
+                                        AudioObjectID inDeviceObjectID,
+                                        const AudioServerPlugInClientInfo* inClientInfo);
+static OSStatus     EFF_RemoveDeviceClient(AudioServerPlugInDriverRef inDriver,
+                                           AudioObjectID inDeviceObjectID,
+                                           const AudioServerPlugInClientInfo* inClientInfo);
+static OSStatus     EFF_PerformDeviceConfigurationChange(AudioServerPlugInDriverRef inDriver,
+                                                         AudioObjectID inDeviceObjectID,
+                                                         UInt64 inChangeAction,
+                                                         void* inChangeInfo);
+static OSStatus     EFF_AbortDeviceConfigurationChange(AudioServerPlugInDriverRef inDriver,
+                                                       AudioObjectID inDeviceObjectID,
+                                                       UInt64 inChangeAction,
+                                                       void* inChangeInfo);
+static Boolean      EFF_HasProperty(AudioServerPlugInDriverRef inDriver,
+                                    AudioObjectID inObjectID,
+                                    pid_t inClientProcessID,
+                                    const AudioObjectPropertyAddress* inAddress);
+static OSStatus     EFF_IsPropertySettable(AudioServerPlugInDriverRef inDriver,
+                                           AudioObjectID inObjectID,
+                                           pid_t inClientProcessID,
+                                           const AudioObjectPropertyAddress* inAddress,
+                                           Boolean* outIsSettable);
+static OSStatus     EFF_GetPropertyDataSize(AudioServerPlugInDriverRef inDriver,
+                                            AudioObjectID inObjectID,
+                                            pid_t inClientProcessID,
+                                            const AudioObjectPropertyAddress* inAddress,
+                                            UInt32 inQualifierDataSize,
+                                            const void* inQualifierData,
+                                            UInt32* outDataSize);
+static OSStatus     EFF_GetPropertyData(AudioServerPlugInDriverRef inDriver,
+                                        AudioObjectID inObjectID,
+                                        pid_t inClientProcessID,
+                                        const AudioObjectPropertyAddress* inAddress,
+                                        UInt32 inQualifierDataSize,
+                                        const void* inQualifierData,
+                                        UInt32 inDataSize,
+                                        UInt32* outDataSize,
+                                        void* outData);
+static OSStatus     EFF_SetPropertyData(AudioServerPlugInDriverRef inDriver,
+                                        AudioObjectID inObjectID,
+                                        pid_t inClientProcessID,
+                                        const AudioObjectPropertyAddress* inAddress,
+                                        UInt32 inQualifierDataSize,
+                                        const void* inQualifierData,
+                                        UInt32 inDataSize,
+                                        const void* inData);
+static OSStatus     EFF_StartIO(AudioServerPlugInDriverRef inDriver,
+                                AudioObjectID inDeviceObjectID,
+                                UInt32 inClientID);
+static OSStatus     EFF_StopIO(AudioServerPlugInDriverRef inDriver,
+                               AudioObjectID inDeviceObjectID,
+                               UInt32 inClientID);
+static OSStatus     EFF_GetZeroTimeStamp(AudioServerPlugInDriverRef inDriver,
+                                         AudioObjectID inDeviceObjectID,
+                                         UInt32 inClientID,
+                                         Float64* outSampleTime,
+                                         UInt64* outHostTime,
+                                         UInt64* outSeed);
+static OSStatus     EFF_WillDoIOOperation(AudioServerPlugInDriverRef inDriver,
+                                          AudioObjectID inDeviceObjectID,
+                                          UInt32 inClientID,
+                                          UInt32 inOperationID,
+                                          Boolean* outWillDo,
+                                          Boolean* outWillDoInPlace);
+static OSStatus     EFF_BeginIOOperation(AudioServerPlugInDriverRef inDriver,
+                                         AudioObjectID inDeviceObjectID,
+                                         UInt32 inClientID,
+                                         UInt32 inOperationID,
+                                         UInt32 inIOBufferFrameSize,
+                                         const AudioServerPlugInIOCycleInfo* inIOCycleInfo);
+static OSStatus     EFF_DoIOOperation(AudioServerPlugInDriverRef inDriver,
+                                      AudioObjectID inDeviceObjectID,
+                                      AudioObjectID inStreamObjectID,
+                                      UInt32 inClientID,
+                                      UInt32 inOperationID,
+                                      UInt32 inIOBufferFrameSize,
+                                      const AudioServerPlugInIOCycleInfo* inIOCycleInfo,
+                                      void* ioMainBuffer,
+                                      void* ioSecondaryBuffer);
+static OSStatus     EFF_EndIOOperation(AudioServerPlugInDriverRef inDriver,
+                                       AudioObjectID inDeviceObjectID,
+                                       UInt32 inClientID,
+                                       UInt32 inOperationID,
+                                       UInt32 inIOBufferFrameSize,
+                                       const AudioServerPlugInIOCycleInfo* inIOCycleInfo);
 
 
 #pragma mark The COM Interface
