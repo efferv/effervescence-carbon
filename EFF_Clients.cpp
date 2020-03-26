ba3f64cc4f0044fe3f68d69599c3404d76325357
nerrons <coniinedieu@gmail.com>
2020-03-26 20:52:58 +0800

    Add EFF_Clients

---
+++ EFF_Clients.cpp
@@ -0,0 +1,403 @@
+//
+//  EFF_Clients.cpp
+//  effervescence-driver
+//
+//  Created by Nerrons on 26/3/20.
+//  Copyright © 2020 nerrons. All rights reserved.
+//
+
+// Self Include
+#include "EFF_Clients.h"
+
+// Local Includes
+#include "EFF_Types.h"
+#include "EFF_PlugIn.h"
+
+// PublicUtility Includes
+#include "CAException.h"
+#include "CACFDictionary.h"
+#include "CADispatchQueue.h"
+
+
+#pragma mark Constructor/Destructor
+
+EFF_Clients::EFF_Clients(AudioObjectID inOwnerDeviceID,
+                         EFF_TaskQueue* inTaskQueue)
+:
+    mOwnerDeviceID(inOwnerDeviceID),
+    mClientMap(inTaskQueue)
+{
+    mRelativeVolumeCurve.AddRange(kAppRelativeVolumeMinRawValue,
+                                  kAppRelativeVolumeMaxRawValue,
+                                  kAppRelativeVolumeMinDbValue,
+                                  kAppRelativeVolumeMaxDbValue);
+}
+
+
+#pragma mark Add/Remove Clients
+
+void    EFF_Clients::AddClient(EFF_Client inClient)
+{
+    CAMutex::Locker theLocker(mMutex);
+
+    // Check whether this is the music player's client
+    bool pidMatchesMusicPlayerProperty =
+        (mMusicPlayerProcessIDProperty != 0 && inClient.mProcessID == mMusicPlayerProcessIDProperty);
+    bool bundleIDMatchesMusicPlayerProperty =
+        (mMusicPlayerBundleIDProperty != "" &&
+         inClient.mBundleID.IsValid() &&
+         inClient.mBundleID == mMusicPlayerBundleIDProperty);
+    inClient.mIsMusicPlayer = (pidMatchesMusicPlayerProperty || bundleIDMatchesMusicPlayerProperty);
+
+    if(inClient.mIsMusicPlayer)
+    {
+        DebugMsg("EFF_Clients::AddClient: Adding music player client. mClientID = %u", inClient.mClientID);
+    }
+
+    mClientMap.AddClient(inClient);
+
+    // If we're adding EFFApp, update our local copy of its client ID
+    if(inClient.mBundleID.IsValid() && inClient.mBundleID == kEFFAppBundleID)
+    {
+        mEFFAppClientID = inClient.mClientID;
+    }
+}
+
+void    EFF_Clients::RemoveClient(const UInt32 inClientID)
+{
+    CAMutex::Locker theLocker(mMutex);
+    
+    EFF_Client theRemovedClient = mClientMap.RemoveClient(inClientID);
+    
+    // If we're removing EFFApp, clear our local copy of its client ID
+    if(theRemovedClient.mClientID == mEFFAppClientID)
+    {
+        mEFFAppClientID = -1;
+    }
+}
+
+
+#pragma mark IO Status
+
+// Start IO for inClientID and update stats.
+// Return true if no other clients were running IO before this one started, which means the device should start IO
+bool    EFF_Clients::StartIONonRT(UInt32 inClientID)
+{
+    CAMutex::Locker theLocker(mMutex);
+    // get client from inClientID
+    EFF_Client theClient;
+    bool didFindClient = mClientMap.GetClientNonRT(inClientID, &theClient);
+    ThrowIf(!didFindClient,
+            EFF_InvalidClientException(),
+            "EFF_Clients::StartIO: Cannot start IO for client that was never added");
+
+    bool didStartIO = false; // the value to be returned
+    bool sendIsRunningNotification = false;
+    bool sendIsRunningSomewhereOtherThanEFFAppNotification = false;
+
+    if(!theClient.mDoingIO)
+    {
+        // Make sure we can start
+        ThrowIf(mStartCount == UINT64_MAX,
+                CAException(kAudioHardwareIllegalOperationError),
+                "EFF_Clients::StartIO: failed to start because the ref count was maxxed out already");
+
+        DebugMsg("EFF_Clients::StartIO: Client %u (%s, %d) starting IO",
+                 inClientID,
+                 CFStringGetCStringPtr(theClient.mBundleID.GetCFString(), kCFStringEncodingUTF8),
+                 theClient.mProcessID);
+        
+        mClientMap.StartIONonRT(inClientID);
+        
+        mStartCount++;
+        
+        // Update mStartCountExcludingEFFApp
+        if(!IsEFFApp(inClientID))
+        {
+            ThrowIf(mStartCountExcludingEFFApp == UINT64_MAX,
+                    CAException(kAudioHardwareIllegalOperationError),
+                    "EFF_Clients::StartIO: failed to start because mStartCountExcludingEFFApp was maxxed out already");
+
+            mStartCountExcludingEFFApp++;
+            
+            if(mStartCountExcludingEFFApp == 1)
+            {
+                sendIsRunningSomewhereOtherThanEFFAppNotification = true;
+            }
+        }
+        
+        didStartIO = (mStartCount == 1);
+        sendIsRunningNotification = didStartIO;
+    }
+    
+    Assert(mStartCountExcludingEFFApp == mStartCount - 1 || mStartCountExcludingEFFApp == mStartCount,
+           "mStartCount and mStartCountExcludingEFFApp are out of sync");
+    
+    SendIORunningNotifications(sendIsRunningNotification,
+                               sendIsRunningSomewhereOtherThanEFFAppNotification);
+
+    return didStartIO;
+}
+
+// Stop IO for inClientID and update stats.
+// Return true if we stopped IO entirely (i.e. there are no clients still running IO)
+bool    EFF_Clients::StopIONonRT(UInt32 inClientID)
+{
+    CAMutex::Locker theLocker(mMutex);
+
+    // get client from inClientID
+    EFF_Client theClient;
+    bool didFindClient = mClientMap.GetClientNonRT(inClientID, &theClient);
+    ThrowIf(!didFindClient,
+            EFF_InvalidClientException(),
+            "EFF_Clients::StopIO: Cannot stop IO for client that was never added");
+    
+    bool didStopIO = false; // the value to be returned
+    bool sendIsRunningNotification = false;
+    bool sendIsRunningSomewhereOtherThanEFFAppNotification = false;
+    
+    if(theClient.mDoingIO)
+    {
+        DebugMsg("EFF_Clients::StopIO: Client %u (%s, %d) stopping IO",
+                 inClientID,
+                 CFStringGetCStringPtr(theClient.mBundleID.GetCFString(), kCFStringEncodingUTF8),
+                 theClient.mProcessID);
+        
+        mClientMap.StopIONonRT(inClientID);
+        
+        ThrowIf(mStartCount <= 0,
+                CAException(kAudioHardwareIllegalOperationError),
+                "EFF_Clients::StopIO: Underflowed mStartCount");
+        
+        mStartCount--;
+        
+        // Update mStartCountExcludingEFFApp
+        if(!IsEFFApp(inClientID))
+        {
+            ThrowIf(mStartCountExcludingEFFApp <= 0,
+                    CAException(kAudioHardwareIllegalOperationError),
+                    "EFF_Clients::StopIO: Underflowed mStartCountExcludingEFFApp");
+            
+            mStartCountExcludingEFFApp--;
+            
+            if(mStartCountExcludingEFFApp == 0)
+            {
+                sendIsRunningSomewhereOtherThanEFFAppNotification = true;
+            }
+        }
+
+        didStopIO = (mStartCount == 0);
+        sendIsRunningNotification = didStopIO;
+    }
+    
+    Assert(mStartCountExcludingEFFApp == mStartCount - 1 || mStartCountExcludingEFFApp == mStartCount,
+           "mStartCount and mStartCountExcludingEFFApp are out of sync");
+    
+    SendIORunningNotifications(sendIsRunningNotification, sendIsRunningSomewhereOtherThanEFFAppNotification);
+    
+    return didStopIO;
+}
+
+// Sends PropertiesChanged notifications for kAudioDevicePropertyDeviceIsRunning and
+// kAudioDeviceCustomPropertyDeviceIsRunningSomewhereOtherThanEFFApp
+void    EFF_Clients::SendIORunningNotifications(bool sendIsRunningNotification,
+                                                bool sendIsRunningSomewhereOtherThanEFFAppNotification)
+const
+{
+    if(sendIsRunningNotification || sendIsRunningSomewhereOtherThanEFFAppNotification)
+    {
+        CADispatchQueue::GetGlobalSerialQueue().Dispatch(false, ^{
+            AudioObjectPropertyAddress theChangedProperties[2];
+            UInt32 theNotificationCount = 0;
+
+            if(sendIsRunningNotification)
+            {
+                DebugMsg("EFF_Clients::SendIORunningNotifications: Sending kAudioDevicePropertyDeviceIsRunning");
+                theChangedProperties[0] = { kAudioDevicePropertyDeviceIsRunning, kAudioObjectPropertyScopeGlobal, kAudioObjectPropertyElementMaster };
+                theNotificationCount++;
+            }
+
+            if(sendIsRunningSomewhereOtherThanEFFAppNotification)
+            {
+                DebugMsg("EFF_Clients::SendIORunningNotifications: Sending kAudioDeviceCustomPropertyDeviceIsRunningSomewhereOtherThanEFFApp");
+                theChangedProperties[theNotificationCount] = kEFFRunningSomewhereOtherThanEFFAppAddress;
+                theNotificationCount++;
+            }
+
+            EFF_PlugIn::Host_PropertiesChanged(mOwnerDeviceID, theNotificationCount, theChangedProperties);
+        });
+    }
+}
+
+#pragma mark Music Player
+
+bool    EFF_Clients::SetMusicPlayer(const pid_t inPID)
+{
+    ThrowIf(inPID < 0,
+            EFF_InvalidClientPIDException(),
+            "EFF_Clients::SetMusicPlayer: Invalid music player PID");
+
+    CAMutex::Locker theLocker(mMutex);
+    
+    if(mMusicPlayerProcessIDProperty == inPID)
+    {
+        // We're not changing the properties, so return false
+        return false;
+    }
+    
+    mMusicPlayerProcessIDProperty = inPID;
+    // Unset the bundle ID property
+    mMusicPlayerBundleIDProperty = "";
+    
+    DebugMsg("EFF_Clients::SetMusicPlayer: Setting music player by PID. inPID=%d", inPID);
+    
+    // Update the clients' mIsMusicPlayer fields
+    mClientMap.UpdateMusicPlayerFlags(inPID);
+    
+    return true;
+}
+
+bool    EFF_Clients::SetMusicPlayer(const CACFString inBundleID)
+{
+    Assert(inBundleID.IsValid(), "EFF_Clients::SetMusicPlayer: Invalid CACFString given as bundle ID");
+    
+    CAMutex::Locker theLocker(mMutex);
+    
+    if(mMusicPlayerBundleIDProperty == inBundleID)
+    {
+        // We're not changing the properties, so return false
+        return false;
+    }
+
+    mMusicPlayerBundleIDProperty = inBundleID;
+    // Unset the PID property
+    mMusicPlayerProcessIDProperty = 0;
+    
+    DebugMsg("EFF_Clients::SetMusicPlayer: Setting music player by bundle ID. inBundleID=%s",
+             CFStringGetCStringPtr(inBundleID.GetCFString(), kCFStringEncodingUTF8));
+    
+    // Update the clients' mIsMusicPlayer fields
+    mClientMap.UpdateMusicPlayerFlags(inBundleID);
+    
+    return true;
+}
+
+bool    EFF_Clients::IsMusicPlayerRT(const UInt32 inClientID)
+const
+{
+    EFF_Client theClient;
+    bool didGetClient = mClientMap.GetClientRT(inClientID, &theClient);
+    return didGetClient && theClient.mIsMusicPlayer;
+}
+
+#pragma mark App Volumes
+
+// not sure if we should differentiate "client not found" and "client doesn't have custom volume"
+Float32 EFF_Clients::GetClientRelativeVolumeRT(UInt32 inClientID)
+const
+{
+    EFF_Client theClient;
+    bool didGetClient = mClientMap.GetClientRT(inClientID, &theClient);
+    return (didGetClient ? theClient.mRelativeVolume : 1.0f);
+}
+
+// same for this one, see GetClientRelativeVolumeRT(UInt32)
+SInt32 EFF_Clients::GetClientPanPositionRT(UInt32 inClientID)
+const
+{
+    EFF_Client theClient;
+    bool didGetClient = mClientMap.GetClientRT(inClientID, &theClient);
+    return (didGetClient ? theClient.mPanPosition : kAppPanCenterRawValue);
+}
+
+bool    EFF_Clients::SetClientsRelativeVolumes(const CACFArray inAppVolumes)
+{
+    bool didChangeAppVolumes = false;
+    
+    // Each element in appVolumes is a CFDictionary containing the process id and/or bundle id of an app, and its
+    // new relative volume
+    for(UInt32 i = 0; i < inAppVolumes.GetNumberItems(); i++)
+    {
+        CACFDictionary theAppVolume(false);
+        inAppVolumes.GetCACFDictionary(i, theAppVolume);
+        
+        // Get the app's PID from the dict
+        pid_t theAppPID;
+        bool didFindPID = theAppVolume.GetSInt32(CFSTR(kEFFAppVolumesKey_ProcessID), theAppPID);
+        
+        // Get the app's bundle ID from the dict
+        CACFString theAppBundleID;
+        theAppBundleID.DontAllowRelease();
+        theAppVolume.GetCACFString(CFSTR(kEFFAppVolumesKey_BundleID), theAppBundleID);
+        
+        ThrowIf(!didFindPID && !theAppBundleID.IsValid(),
+                EFF_InvalidClientRelativeVolumeException(),
+                "EFF_Clients::SetClientsRelativeVolumes: App volume was sent without PID or bundle ID for app");
+        
+        bool didGetVolume;
+        {
+            SInt32 theRawRelativeVolume;
+            didGetVolume = theAppVolume.GetSInt32(CFSTR(kEFFAppVolumesKey_RelativeVolume), theRawRelativeVolume);
+            
+            if (didGetVolume) {
+                ThrowIf(didGetVolume &&
+                        (theRawRelativeVolume < kAppRelativeVolumeMinRawValue ||
+                         theRawRelativeVolume > kAppRelativeVolumeMaxRawValue),
+                        EFF_InvalidClientRelativeVolumeException(),
+                        "EFF_Clients::SetClientsRelativeVolumes: Relative volume for app out of valid range");
+                
+                // Apply the volume curve to the raw volume
+                //
+                // mRelativeVolumeCurve uses the default kPow2Over1Curve transfer function, so we also multiply by 4 to
+                // keep the middle volume equal to 1 (meaning apps' volumes are unchanged by default).
+                Float32 theRelativeVolume = mRelativeVolumeCurve.ConvertRawToScalar(theRawRelativeVolume) * 4;
+
+                // Try to update the client's volume, first by PID and then by bundle ID. Always try
+                // both because apps can have multiple clients.
+                if(mClientMap.SetClientsRelativeVolume(theAppPID, theRelativeVolume))
+                {
+                    didChangeAppVolumes = true;
+                }
+
+                if(mClientMap.SetClientsRelativeVolume(theAppBundleID, theRelativeVolume))
+                {
+                    didChangeAppVolumes = true;
+                }
+
+                // TODO: If the app isn't currently a client, we should add it to the past clients
+                //       map, or update its past volume if it's already in there.
+            }
+        }
+        
+        bool didGetPanPosition;
+        {
+            SInt32 thePanPosition;
+            didGetPanPosition = theAppVolume.GetSInt32(CFSTR(kEFFAppVolumesKey_PanPosition), thePanPosition);
+            if (didGetPanPosition) {
+                ThrowIf(didGetPanPosition && (thePanPosition < kAppPanLeftRawValue || thePanPosition > kAppPanRightRawValue),
+                                              EFF_InvalidClientPanPositionException(),
+                                              "EFF_Clients::SetClientsRelativeVolumes: Pan position for app out of valid range");
+                
+                if(mClientMap.SetClientsPanPosition(theAppPID, thePanPosition))
+                {
+                    didChangeAppVolumes = true;
+                }
+
+                if(mClientMap.SetClientsPanPosition(theAppBundleID, thePanPosition))
+                {
+                    didChangeAppVolumes = true;
+                }
+
+                // TODO: If the app isn't currently a client, we should add it to the past clients
+                //       map, or update its past pan position if it's already in there.
+            }
+        }
+        
+        ThrowIf(!didGetVolume && !didGetPanPosition,
+                EFF_InvalidClientRelativeVolumeException(),
+                "EFF_Clients::SetClientsRelativeVolumes: No volume or pan position in request");
+    }
+    
+    return didChangeAppVolumes;
+}
+
---
+++ EFF_Clients.h
@@ -0,0 +1,148 @@
+//
+//  EFF_Clients.h
+//  effervescence-core
+//
+//  Created by Nerrons on 26/3/20.
+//  Copyright © 2020 nerrons. All rights reserved.
+//
+
+#ifndef EFF_Clients_h
+#define EFF_Clients_h
+
+// Local Includes
+#include "EFF_Client.h"
+#include "EFF_ClientMap.h"
+
+// PublicUtility Includes
+#include "CAVolumeCurve.h"
+#include "CAMutex.h"
+#include "CACFArray.h"
+
+// System Includes
+#include <CoreAudio/AudioServerPlugIn.h>
+
+
+// Forward Declations
+class EFF_ClientTasks;
+
+
+#pragma clang assume_nonnull begin
+
+//==================================================================================================
+//    EFF_Clients
+//
+//  Holds information about the clients (of the host) of the EFFDevice, i.e. the apps registered
+//  with the HAL, generally so they can do IO at some point. EFFApp and the music player are special
+//  case clients.
+//
+//  Methods whose names end with "RT" should only be called from real-time threads.
+//==================================================================================================
+
+
+class EFF_Clients
+{
+    friend class EFF_ClientTasks;
+
+
+#pragma mark Constructor/Destructor
+
+public:
+                                EFF_Clients(AudioObjectID inOwnerDeviceID, EFF_TaskQueue* inTaskQueue);
+                                ~EFF_Clients() = default;
+                                // Disallow copying. (It could make sense to implement these in future,
+                                // but we don't need them currently.)
+                                EFF_Clients(const EFF_Clients&) = delete;
+                                EFF_Clients& operator=(const EFF_Clients&) = delete;
+    
+    void                        AddClient(EFF_Client inClient);
+    void                        RemoveClient(const UInt32 inClientID);
+    
+
+#pragma mark API
+    bool                        ClientsRunningIO() const;
+                                    { return mStartCount > 0; }
+    bool                        ClientsOtherThanEFFAppRunningIO() const;
+                                    { return mStartCountExcludingBGMApp > 0; }
+    bool                        IsEFFApp(UInt32 inClientID) const
+                                    { return inClientID == mEFFAppClientID; }
+    bool                        EFFAppHasClientRegistered() const
+                                    { return mEFFAppClientID != -1; }
+    
+    // >>> Music player API <<<
+    inline pid_t                GetMusicPlayerProcessIDProperty() const
+                                    { return mMusicPlayerProcessIDProperty; }
+    inline CFStringRef          CopyMusicPlayerBundleIDProperty() const
+                                    { return mMusicPlayerBundleIDProperty.CopyCFString(); }
+    bool                        IsMusicPlayerRT(const UInt32 inClientID) const;
+    // Returns true if the PID was changed
+    bool                        SetMusicPlayer(const pid_t inPID);
+    // Returns true if the bundle ID was changed
+    bool                        SetMusicPlayer(const CACFString inBundleID);
+    
+    // >>> Volume API <<<
+    Float32                     GetClientRelativeVolumeRT(UInt32 inClientID) const;
+    SInt32                      GetClientPanPositionRT(UInt32 inClientID) const;
+    // Copies the current and past clients into an array in the format expected for
+    // kAudioDeviceCustomPropertyAppVolumes. (Except that CACFArray and CACFDictionary are used instead
+    // of unwrapped CFArray and CFDictionary refs.)
+    CACFArray                   CopyClientRelativeVolumesAsAppVolumes() const
+                                    { return mClientMap.CopyClientRelativeVolumesAsAppVolumes(mRelativeVolumeCurve); };
+    // inAppVolumes is an array of dicts with the keys kEFFAppVolumesKey_ProcessID,
+    // kEFFAppVolumesKey_BundleID and optionally kEFFAppVolumesKey_RelativeVolume and
+    // kEFFAppVolumesKey_PanPosition. This method finds the client for
+    // each app by PID or bundle ID, sets the volume and applies mRelativeVolumeCurve to it.
+    //
+    // Returns true if any clients' relative volumes were changed.
+    bool                        SetClientsRelativeVolumes(const CACFArray inAppVolumes);
+    
+    
+#pragma mark Implementation
+private:
+    // Only EFF_TaskQueue is allowed to call these (through the EFF_ClientTasks interface). We get notifications
+    // from the HAL when clients start/stop IO and they have to be processed in the order we receive them to
+    // avoid race conditions. If these methods could be called directly those calls would skip any queued calls.
+    bool                        StartIONonRT(UInt32 inClientID);
+    bool                        StopIONonRT(UInt32 inClientID);
+    
+    void                        SendIORunningNotifications(bool sendIsRunningNotification,
+                                                           bool sendIsRunningSomewhereOtherThanEFFAppNotification) const;
+    
+#pragma mark Members
+    AudioObjectID               mOwnerDeviceID;
+    EFF_ClientMap               mClientMap;
+
+    // Counters for the number of clients that are doing IO. These are used to tell whether any clients
+    // are currently doing IO without having to check every client's mDoingIO.
+    //
+    // We need to reference count this rather than just using a bool because the HAL might (but usually
+    // doesn't) call our StartIO/StopIO functions for clients other than the first to start and last to
+    // stop.
+    UInt64                      mStartCount = 0;
+    UInt64                      mStartCountExcludingEFFApp = 0;
+    
+    CAMutex                     mMutex { "Clients" };
+    
+    SInt64                      mEFFAppClientID = -1;
+    
+    // The value of the kAudioDeviceCustomPropertyMusicPlayerProcessID property, or 0 if it's unset/null.
+    // We store this separately because the music player might not always be a client, but could be added
+    // as one at a later time.
+    pid_t                       mMusicPlayerProcessIDProperty = 0;
+    
+    // The value of the kAudioDeviceCustomPropertyMusicPlayerBundleID property, or the empty string it it's
+    // unset/null. UTF8 encoding.
+    //
+    // As with mMusicPlayerProcessID, we keep a copy of the bundle ID the user sets for the music player
+    // because there might be no client with that bundle ID. In that case we need to be able to give the
+    // property's value if the HAL asks for it, and to recognise the music player if it's added a client.
+    CACFString                  mMusicPlayerBundleIDProperty { "" };
+    
+    // The volume curve we apply to raw client volumes before they're used
+    CAVolumeCurve               mRelativeVolumeCurve;
+    
+};
+
+#pragma clang assume_nonnull end
+
+
+#endif /* EFF_Clients_h */
